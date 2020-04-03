1dcede9fc82a0d50d091037e9b809bc0d0173bc7
Leonardo Banchi <benkj.it@gmail.com>
2020-04-03 09:18:18 +0200

    favicons

---
+++ assets/makeicons.sh
@@ -0,0 +1,6 @@
+convert leo.jpeg -resize 576x576 icons/icon@3x.png  
+convert leo.jpeg -resize 384x384 icons/icon@2x.png  
+convert leo.jpeg -resize 192x192 icons/icon.png  
+convert leo.jpeg -resize 144x144 icons/icon@0,75x.png  
+convert leo.jpeg -resize 96x96   icons/icon@0,5x.png  
+convert leo.jpeg -resize 48x48   icons/icon@0,25x.png  
